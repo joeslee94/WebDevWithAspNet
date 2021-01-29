5de0ad26490a5ebb05f04c5fcd5de4e02ac8675d
Joe Lee <75293272+joeslee94@users.noreply.github.com>
2021-01-30 03:15:57 +0900

    Add files via upload

---
+++ 1. First Web App/CS-ASP_003 MyFirstWebApp/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void firstNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitClick(object sender, EventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            resultLabel.Text = firstName;
+
+            //string firstName;
+            //firstName = "Bob";
+
+            string lastName = lastNameTextBox.Text;
+
+            string result = $"Hello {firstName} {lastName}";
+
+            resultLabel.Text = result;
+        }
+    }
+}
